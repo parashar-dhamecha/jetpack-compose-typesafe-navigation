43b4a3d7b2c9356fab8ab3d7c0016d3ed5bea214
parashar-dhamecha <GitHub_p@55w0rd>
2024-10-19 16:53:34 +0530

    initial commit

---
+++ app/src/main/java/com/innotechie/jetpack_compose_typesafe_navigation/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.innotechie.jetpack_compose_typesafe_navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.innotechie.jetpack_compose_typesafe_navigation.ui.theme.JetpackcomposetypesafenavigationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackcomposetypesafenavigationTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackcomposetypesafenavigationTheme {
+        Greeting("Android")
+    }
+}
